15ec0cbc9e23a652417b37b5e4bec946898917c4
Victor Chaplygin <guyswinter0@gmail.com>
2024-02-05 07:27:22 +0300

    Changed icon and app title

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -162,7 +162,7 @@ class _HomeState extends State<Home> {
       backgroundColor: tdFalcon,
       title: const Center(
           child: Text(
-            "My ToDo App", style: TextStyle(
+            "Тудушечка", style: TextStyle(
               color: tdWhite
             ),
           )
